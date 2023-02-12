4f7ee167a120f2eb561d9f23d1a75a1324e6ad55
machego <114348981+machego@users.noreply.github.com>
2023-02-12 22:41:12 +0100

    Soloproject scrimba
    challenge scrimba soloproject
    design spec: figma file
    Generate all conversion via button click

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const btn = document.getElementById('btn');
+const inputEl = document.getElementById('input-el');
+
+const genLength = document.getElementById('gen-length');
+const genVolume = document.getElementById('gen-volume');
+const genMass = document.getElementById('gen-mass');
+
+//let num = 0 
+
+btn.addEventListener('click', () => {
+    num = inputEl.value 
+    
+    lengthGen();
+    volumeGen();
+    massGen();
+    inputEl.value = ""
+})
+
+function lengthGen() {
+    let meters = ((inputEl.value) * 0.3048)
+    let feet = ((inputEl.value) * 3.281)
+    genLength.innerHTML = `${inputEl.value} meters = ${feet} feet | ${inputEl.value} feet = ${meters} meters`;
+}
+
+function volumeGen() {
+    let liters = ((inputEl.value) * 3.785)
+    let gallons = ((inputEl.value) * 0.264)
+    genVolume.innerHTML = `${inputEl.value} liters = ${gallons} gallons | ${inputEl.value} gallons = ${liters} liters`;
+}
+
+function massGen() {
+    let kilos = ((inputEl.value) * 0.454)
+    let pounds = ((inputEl.value) * 2.204)
+    genMass.innerHTML = `${inputEl.value} kilos = ${pounds} pounds | ${inputEl.value} gallons = ${kilos} kilos`;
+}
